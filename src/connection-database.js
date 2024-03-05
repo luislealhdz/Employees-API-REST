dcea9a30f506fb943ed747bbfa76b91f98f022cf
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-03-04 20:45:15 -0600

    🆕 Change to postgresql database

--- src/connection-database.js
+++ src/connection-database.js
@@ -1,4 +1,5 @@
 import { createPool } from "mysql2/promise";
+import pg from "pg";
 
 import {
     DB_HOST,
@@ -8,10 +9,19 @@ import {
     DB_DATABASE,
 } from "./config.js";
 
-export const pool = createPool({
+// export const pool = createPool({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     port: DB_PORT,
+//     database: DB_DATABASE,
+// });
+
+export const pool = new pg.Pool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     port: DB_PORT,
     database: DB_DATABASE,
+    ssl: true,
 });
