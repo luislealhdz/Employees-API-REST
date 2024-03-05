d6827798acf5e41d37e58ef2b8625de70e262560
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-03-04 19:36:20 -0600

    Employees API

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import indexRoutes from "./routes/index.routes.js";
+import employeesRoutes from "./routes/employees.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", indexRoutes);
+app.use("/api", employeesRoutes);
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Endpoint not found",
+    });
+});
+
+export default app;
---
+++ src/routes/employees.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+    getEmployee,
+    getEmployees,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee,
+} from "../controllers/employees.controller.js";
+
+const router = Router();
+
+router.get("/employees/:id", getEmployee);
+
+router.get("/employees", getEmployees);
+
+router.post("/employees", createEmployee);
+
+router.patch("/employees/:id", updateEmployee);
+
+router.delete("/employees/:id", deleteEmployee);
+
+export default router;
